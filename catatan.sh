ad9639eb39acf735b13043cd317d8ac8fc7b463d
Harwinsyah <haris.harwinsyah@gmail.com>
2023-01-04 17:08:45 +0800

    Commit Pertama

---
+++ catatan.sh
@@ -0,0 +1,10 @@
+GET
+    http://127.0.0.1:8000/api/posts/
+    http://127.0.0.1:8000/api/posts/1
+
+POST
+    http://127.0.0.1:8000/api/posts/1
+    http://127.0.0.1:8000/api/posts/
+
+DELETE
+    http://127.0.0.1:8000/api/posts/1    
